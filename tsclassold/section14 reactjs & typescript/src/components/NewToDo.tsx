afa80f490e1db10b43aeef75f8d37bff715a7e62
Christopher <56417034+lhxo@users.noreply.github.com>
2024-07-17 15:27:14 -0700

    Add files via upload
    section 14 so far

---
+++ tsclassold/section14 reactjs & typescript/src/components/NewToDo.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react'
+
+type NewToDoProps = {
+    onAddToDo: (todoText:string) => void
+}
+
+const NewToDo: React.FC<NewToDoProps> = props => {
+    const textInputRef = useRef<HTMLInputElement>(null)
+
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        props.onAddToDo(enteredText)
+    }
+
+    return (
+        <form onSubmit={todoSubmitHandler}>
+            <div>
+                <label htmlFor="todo-text">To do Text</label>
+                <input type="text" id="todo-text" ref={textInputRef}/>
+            </div>
+            <button type="submit">Add ToDo</button>
+        </form>
+    )
+};
+
+export default NewToDo
