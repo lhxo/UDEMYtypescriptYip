f54cd13b71ce703980504a2d5efb8708b2b3b1b8
Christopher <56417034+lhxo@users.noreply.github.com>
2024-07-18 14:35:41 -0700

    Delete tsclassold directory
    Restructuring - start

--- tsclassold/section10 modules&namespaces/src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list.js";
-
-//Instantiate project
-new ProjectInput()
-new ProjectList('active');
-new ProjectList('finished');
--- tsclassold/section11 using webpack with typescript/src/app.ts
+++
@@ -1,7 +0,0 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
-
-//Instantiate project
-new ProjectInput()
-new ProjectList('active');
-new ProjectList('finished');
